f78761b11008cb1cb2128d0eaa063c8371634f30
Luat Dinh Bui <l.dinh-bui@ovan.de>
2025-07-15 01:28:08 +0200

    [WIP] Extension Base

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Banner Extension',
+    'description' => 'A TYPO3 extension adding flash notification-like banners in pages',
+    'category' => 'plugin',
+    'author' => 'Luat Dinh Bui',
+    'author_email' => 'ldinhbui@gmail.com',
+    'state' => 'alpha',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.0.0-13.4.99',
+            'bootstrap_package' => '15.0.0-15.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
