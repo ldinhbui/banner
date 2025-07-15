be66e4ed20eecc611963cbfb289dd10af7095712
Luat Dinh Bui <l.dinh-bui@ovan.de>
2025-07-15 03:03:58 +0200

    [WIP] CE:Banner Setup

---
+++ Configuration/TCA/Overrides/tt_content_banner.php
@@ -0,0 +1,34 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') || die();
+
+
+$key = 'banner';
+
+ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'label' => 'LLL:EXT:banner/Resources/Private/Language/locallang_be.xlf:wizard.banner.title',
+        'value' => $key,
+        'group' => 'default',
+        'icon' => 'banner-logo',
+    ],
+);
+
+
+$GLOBALS['TCA']['tt_content']['types'][$key] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            header,
+            bodytext,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        ',
+];
